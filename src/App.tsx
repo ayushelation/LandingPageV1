9956677c59b305630b402fe96cef22591ab6d5ca
ayushelation <ayush@elationsoft.net>
2025-12-22 17:45:39 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
+// Remove __BASE_PATH__ or define it properly
+const basePath = import.meta.env.BASE_URL || '/';
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
-      <BrowserRouter basename={__BASE_PATH__}>
+      <BrowserRouter basename={basePath}>
         <AppRoutes />
       </BrowserRouter>
     </I18nextProvider>
